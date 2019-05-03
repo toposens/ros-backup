6543c354850f3cbe1348984f15861a1b21b9ae5a
Sebastian Dengler <dengler@toposens.com>
2019-05-03 18:59:00 +0200

    update travis.sh

--- travis.sh
+++ travis.sh
@@ -79,8 +79,15 @@ ls -al
 #echo "$(cat toposens_driver.info)"
 
 #cd ../../..
+
+echo "--"
+lcov --list toposens_driver.info
+echo "---"
+lcov --list toposens_driver.info.cleaned
+echo "----"
+
 echo $PWD
 echo $CODECOV_TOKEN
 CODECOV_TOKEN="39fdfe66-5f8c-468e-b68f-4d6529702b14"
 #bash <(curl -s https://codecov.io/bash) -X gcov -s catkin_ws/build/coverage -f toposens_driver.info -R ycatkin_ws/src/ts-ros -v
-bash <(curl -s https://codecov.io/bash) -X gcov -f toposens_driver.info.cleaned -v
+bash <(curl -s https://codecov.io/bash) -X gcov -f toposens_driver.info.cleaned
